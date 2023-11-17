f42d5c9bef082a1a02e114958b3088d5b6c79992
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-16 22:17:52 -0300

    criado mixin validation
    - criado mixin validation para componentizar a validação

---
+++ prova_flutter/lib/src/shared/validation/validation.dart
@@ -0,0 +1,31 @@
+mixin Validation {
+  String? fieldValidator(List<String? Function()> validators) {
+    for (final function in validators) {
+      final validation = function();
+      if (validation != null) return validation;
+    }
+    return null;
+  }
+
+  String? isNotEmptyValidator(String? input, [String? message]) {
+    if (input!.isEmpty) return message ?? "O campo deve ser preenchido";
+    return null;
+  }
+
+  String? hasTwoCharValidator(String? input, [String? message]) {
+    if (input!.length < 2) return message ?? "O campo deve ter pelo menos dois caracteres";
+    return null;
+  }
+
+  String? isAlphanumericValidator(String? input, [String? message]) {
+    if ( !RegExp(r'^[a-zA-Z0-9]+$').hasMatch(input!)) return message ?? "O campo deve conter apenas letras e números";
+    return null;
+  }
+
+  String? isNotEndsWithSpaceValidator(String? input, [String? message]) {
+    if (input!.endsWith(' ')) return message ?? "O campo não pode terminar com espaços no final";
+    return null;
+  }
+
+
+}
