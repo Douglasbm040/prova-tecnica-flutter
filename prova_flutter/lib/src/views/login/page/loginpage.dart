dcaa3e7e4a3c68fa06883c9fa8edff760d448782
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-16 22:19:54 -0300

    criado view para login page

--- prova_flutter/lib/src/views/login/page/loginpage.dart
+++ prova_flutter/lib/src/views/login/page/loginpage.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import '../../../custom/customtheme.dart';
+import '../components/textformfieldcomponent.dart';
+import '../controller/logincontroller.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +14,82 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final loginController = LoginController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          width: width,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: CustomTheme.gradientColorBackground,
+            ),
+          ),
+          child: Form(
+            key: loginController.formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Expanded(
+                  flex: 2,
+                  child: SizedBox(),
+                ),
+                TextFormFieldComponent(
+                    label: "Usuário",
+                    width: width,
+                    icon: const Icon(Icons.person),
+                    validator: loginController.userValidation),
+                const SizedBox(height: 20),
+                Observer(builder: (context) {
+                  return TextFormFieldComponent(
+                      label: "Senha",
+                      width: width,
+                      icon: const Icon(Icons.lock),
+                      passwordField: true,
+                      isObscure: loginController.isvisible,
+                      validator: loginController.passwordValidation,
+                      changedVisibility: loginController.changeVisible);
+                }),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: SizedBox(
+                    width: 250,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () => loginController.validateForm(context),
+                      child: Text(
+                        "Entrar",
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                    flex: 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 30.0),
+                          child: Text(
+                            "Políticas de Privacidade",
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
