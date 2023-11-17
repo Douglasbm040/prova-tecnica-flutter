445f0cb1c79f253eb9d926146adca349288489d9
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-16 22:16:56 -0300

    Criado Routes
    - Criação de rotas Nomeadas
    - Centralização dos nomes das rotas de forma estática

--- prova_flutter/lib/src/app.dart
+++ prova_flutter/lib/src/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:prova_flutter/src/shared/routes/routes.dart';
 
 import 'custom/customtheme.dart';
 import 'views/home/page/homepage.dart';
+import 'views/login/page/loginpage.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,9 +11,15 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Prova Tecnica Flutter',
       theme: CustomTheme.getTheme(),
-      home: const HomePage(title: 'Prova Técnica Flutter'),
+      initialRoute: Routes.login,
+      routes: {
+        Routes.home: (context) =>
+            const HomePage(title: 'Flutter Demo Home Page'),
+        Routes.login: (context) => const LoginPage(),
+      },
     );
   }
 }
