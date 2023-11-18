e20702fd304f410b5c7524b735b1382043460952
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 02:23:48 -0300

    Update sharedpreference.dart
    - criado o repository para inserir e obter dados do sharedpreference

--- prova_flutter/lib/src/repository/persistence/sharedpreference.dart
+++ prova_flutter/lib/src/repository/persistence/sharedpreference.dart
@@ -1,5 +1,18 @@
-import 'package:prova_flutter/src/repository/persistence/adapterpersistence.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPreference implements AdapterPersistentence{
-  
+class SharedPreference {
+  List<String> anotations = [];
+
+  Future<void> readAnotations() async {
+    anotations.clear();
+    final sharedPreferences = await SharedPreferences.getInstance();
+    anotations = sharedPreferences.getStringList('anotations') ?? [];
+    print(anotations);
+  }
+
+  Future<void> setAnotations(List<String> anotations) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setStringList('anotations', anotations);
+    // Atualiza a lista após a modificação
+  }
 }
