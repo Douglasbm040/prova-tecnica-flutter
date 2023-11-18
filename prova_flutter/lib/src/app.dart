3d393de5c9f5bc0ec45917687b249ffb527713be
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 14:46:14 -0300

    Update app.dart
    - injetando dependencia via provider

--- prova_flutter/lib/src/app.dart
+++ prova_flutter/lib/src/app.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:prova_flutter/src/repository/persistence/persistence.dart';
 import 'package:prova_flutter/src/shared/routes/routes.dart';
+import 'package:prova_flutter/src/views/home/controller/homecontroler.dart';
+import 'package:prova_flutter/src/views/login/controller/logincontroller.dart';
+import 'package:prova_flutter/src/views/shared/controller/generalcontroller.dart';
+import 'package:provider/provider.dart';
 
 import 'custom/customtheme.dart';
 import 'views/home/page/homepage.dart';
@@ -17,8 +22,22 @@ class App extends StatelessWidget {
       initialRoute: Routes.home,
       routes: {
         Routes.home: (context) =>
-            const HomePage(title: 'Flutter Demo Home Page'),
-        Routes.login: (context) => const LoginPage(),
+            Consumer2<GeneralController, SharedPreference>(
+                builder: (context, generalController, sharedPreferences, _) {
+              return HomePage(
+                controllerGeneral: generalController,
+                controllerPage: HomeController(
+                    sharedPreferences: sharedPreferences,
+                    textController: TextEditingController()),
+              );
+            }),
+        Routes.login: (context) => Consumer<GeneralController>(
+                builder: (context, generalController, _) {
+              return LoginPage(
+                controllerGeneral: generalController,
+                controllerPage: LoginController(),
+              );
+            }),
       },
     );
   }
