49503f0e51ac5bb22cced837c1692339225d59ce
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 14:35:00 -0300

    aplicado a interface adapter e provider
    - usado changenotfier para injeção de dependência
    - usado a interface adapterpersistence para adaptar código de terceiro

--- prova_flutter/lib/src/repository/persistence/persistence.dart
+++ prova_flutter/lib/src/repository/persistence/persistence.dart
@@ -1,15 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:prova_flutter/src/repository/persistence/adapterpersistence.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPreference {
+
+class SharedPreference extends ChangeNotifier implements AdapterPersistentence {
+ 
   List<String> anotations = [];
 
+  @override
   Future<void> readAnotations() async {
     anotations.clear();
     final sharedPreferences = await SharedPreferences.getInstance();
     anotations = sharedPreferences.getStringList('anotations') ?? [];
-    print(anotations);
   }
 
+  @override
   Future<void> setAnotations(List<String> anotations) async {
     final sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setStringList('anotations', anotations);
