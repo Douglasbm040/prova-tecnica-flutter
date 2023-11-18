c38e2e31a4e1d5c10760a3a2f9666954d96569e8
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 14:46:46 -0300

    Update main.dart
    - implementação do provider

--- prova_flutter/lib/main.dart
+++ prova_flutter/lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:prova_flutter/src/app.dart';
+import 'package:prova_flutter/src/repository/persistence/persistence.dart';
+import 'package:prova_flutter/src/repository/redirect/redirect.dart';
+import 'package:prova_flutter/src/views/shared/controller/generalcontroller.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<GeneralController>(
+          create: (context) => GeneralController(redirect: Redirect()),
+        ),
+        ChangeNotifierProvider<SharedPreference>(
+          create: (context) => SharedPreference(),
+        ),
+      ],child: const App()));
 }
  
