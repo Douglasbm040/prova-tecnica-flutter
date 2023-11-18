29f44c65d6844fe298add0b736eeee1f2daff7f8
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 02:10:46 -0300

    criado view para homepage
    - criado layout baseado no mockup
    - adicionado componene de texto adaptativo ao tamanho do input do usuário

--- prova_flutter/lib/src/views/home/page/homepage.dart
+++ prova_flutter/lib/src/views/home/page/homepage.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:prova_flutter/src/custom/customtheme.dart';
+import 'package:prova_flutter/src/views/home/controller/homecontroler.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:prova_flutter/src/views/shared/components/privacepolitictextcomponent.dart';
+
+import '../controller/anotationcontroler.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -10,39 +16,144 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final homeController = HomeController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          width: width,
+          height: height,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: CustomTheme.gradientColorBackground,
+          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SafeArea(
+                child: Card(
+                  child: SizedBox(
+                    width: width * .8,
+                    height: height * .55,
+                    child: Observer(builder: (context) {
+                      return homeController.items.isEmpty
+                          ? const Center(
+                              child: Text(
+                                  "Você ainda não adicionou anotações"),
+                            )
+                          : ListView.builder(
+                              itemCount: homeController.items.length,
+                              itemBuilder: (context, index) {
+                                final anotationController =
+                                    homeController.items[index];
+                                return Card(
+                                    elevation: 3,
+                                    child: Column(
+                                      children: [
+                                        SizedBox(
+                                          width: width * .8,
+                                          child: Stack(
+                                            children: [
+                                              Observer(builder: (context) {
+                                                return AnimatedContainer(
+                                                  curve: Curves.easeInOut,
+                                                  duration: const Duration(
+                                                      seconds: 1),
+                                                  width: 240,
+                                                  height: anotationController
+                                                          .anotation
+                                                          .isNotExpansed
+                                                      ? 80
+                                                      : 350,
+                                                  child: InkWell(
+                                                    onTap: anotationController
+                                                        .anotationChangeIsExpanded,
+                                                    child: Text(
+                                                      anotationController
+                                                          .anotation.text,
+                                                      maxLines:
+                                                          anotationController
+                                                                  .anotation
+                                                                  .isNotExpansed
+                                                              ? null
+                                                              : 3,
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                    ),
+                                                  ),
+                                                );
+                                              }),
+                                              Positioned(
+                                                right: 10,
+                                                child: Row(
+                                                  children: [
+                                                    Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              8.0),
+                                                      child: InkWell(
+                                                          onTap: () =>
+                                                              homeController
+                                                                  .switchMethodeForEdit(
+                                                                      index),
+                                                          child: const Icon(
+                                                              Icons.edit)),
+                                                    ),
+                                                    InkWell(
+                                                      onTap: () => homeController
+                                                          .showDeleteConfirmationDialog(
+                                                              context, index),
+                                                      child: const Icon(
+                                                        Icons.close_rounded,
+                                                        color: Colors.red,
+                                                      ),
+                                                    )
+                                                  ],
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ));
+                              });
+                    }),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 0,
+              ),
+              SizedBox(
+                width: width * 0.8,
+                child: TextField(
+                  controller: homeController.textController,
+                  focusNode: _focusNode,
+                  decoration: InputDecoration(
+                    hintText: "Digite seu nome",
+                    filled: true,
+                    fillColor: Theme.of(context).colorScheme.background,
+                    border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  onSubmitted: homeController.editAnotationText,
+                ),
+              ),
+              PrivacePoliticTextComponent(
+                redirect: () async {
+                  homeController.redirectWeb();
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
