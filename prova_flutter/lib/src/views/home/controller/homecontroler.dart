037492f1b1ec4e78f028af37e5073b5d5e500300
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 02:22:06 -0300

    criado mobx para homepage
    - gerenciamento da lista de anotações

--- prova_flutter/lib/src/views/home/controller/homecontroler.dart
+++ prova_flutter/lib/src/views/home/controller/homecontroler.dart
@@ -1,8 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:mobx/mobx.dart';
+import 'package:prova_flutter/src/model/anotation.dart';
+import 'package:prova_flutter/src/shared/validation/validation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../repository/persistence/sharedpreference.dart';
+import '../../../repository/redirect/adapterredirect.dart';
+import '../../../repository/redirect/urllauncher.dart';
+import '../components/deleteconfirmationdialog.dart';
+import 'anotationcontroler.dart';
 part 'homecontroler.g.dart';
 
 class HomeController = _HomeControllerBase with _$HomeController;
 
-abstract class _HomeControllerBase with Store {
-  
+abstract class _HomeControllerBase with Store, Validation {
+  final TextEditingController textController = TextEditingController();
+  SharedPreference sharedPreferences = SharedPreference();
+  final AdapterRedirect redirect = UrlLauncher();
+  int _index = 0;
+
+  set index(int value) {
+    _index = value;
+  }
+
+  @observable
+  bool isEditionAnotation = false;
+  @observable
+  ObservableList<AnotationController> items =
+      ObservableList<AnotationController>();
+
+  @action
+  void _addAnotation(String? input) {
+    items.add(AnotationController(anotation: Anotation(text: input ?? "")));
+    textController.text = "";
+    updateListSharedPreference();
+  }
+
+  @action
+  void deleteAnotation(int index) {
+    items.removeAt(index);
+    isEditionAnotation = false;
+    textController.text = "";
+    updateListSharedPreference();
+  }
+
+  @action
+  void switchMethodeForEdit(int index) {
+    isEditionAnotation = true;
+    _index = index;
+    textController.text = items[index].anotation.text;
+  }
+
+  void editAnotationText(String input) {
+    if (isEditionAnotation) {
+      items[_index].anotationChangeText(input);
+      isEditionAnotation = false;
+      textController.text = "";
+      updateListSharedPreference();
+      return;
+    }
+    _addAnotation(input);
+  }
+
+  void redirectWeb() {
+    redirect.redirect("https://www.google.com");
+  }
+
+  _HomeControllerBase() {
+    updateListAnotations();
+  }
+  @action
+  void updateListAnotations() async {
+    await sharedPreferences.readAnotations();
+    items = sharedPreferences.anotations
+        .map((text) => AnotationController(anotation: Anotation(text: text)))
+        .toList()
+        .asObservable();
+    
+  }
+
+  void updateListSharedPreference() async {
+    await sharedPreferences
+        .setAnotations(items.map((e) => e.anotation.text).toList());
+    updateListAnotations();
+  }
+
+  void showDeleteConfirmationDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return DeleteConfirmationDialog(
+          onConfirm: () => deleteAnotation(index),
+        );
+      },
+    );
+  }
 }
---
+++ prova_flutter/lib/src/views/home/controller/homecontroler.g.dart
@@ -0,0 +1,87 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'homecontroler.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$HomeController on _HomeControllerBase, Store {
+  late final _$isEditionAnotationAtom =
+      Atom(name: '_HomeControllerBase.isEditionAnotation', context: context);
+
+  @override
+  bool get isEditionAnotation {
+    _$isEditionAnotationAtom.reportRead();
+    return super.isEditionAnotation;
+  }
+
+  @override
+  set isEditionAnotation(bool value) {
+    _$isEditionAnotationAtom.reportWrite(value, super.isEditionAnotation, () {
+      super.isEditionAnotation = value;
+    });
+  }
+
+  late final _$itemsAtom =
+      Atom(name: '_HomeControllerBase.items', context: context);
+
+  @override
+  ObservableList<AnotationController> get items {
+    _$itemsAtom.reportRead();
+    return super.items;
+  }
+
+  @override
+  set items(ObservableList<AnotationController> value) {
+    _$itemsAtom.reportWrite(value, super.items, () {
+      super.items = value;
+    });
+  }
+
+  late final _$_HomeControllerBaseActionController =
+      ActionController(name: '_HomeControllerBase', context: context);
+
+  @override
+  void _addAnotation(String? input) {
+    final _$actionInfo = _$_HomeControllerBaseActionController.startAction(
+        name: '_HomeControllerBase._addAnotation');
+    try {
+      return super._addAnotation(input);
+    } finally {
+      _$_HomeControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void deleteAnotation(int index) {
+    final _$actionInfo = _$_HomeControllerBaseActionController.startAction(
+        name: '_HomeControllerBase.deleteAnotation');
+    try {
+      return super.deleteAnotation(index);
+    } finally {
+      _$_HomeControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void switchMethodeForEdit(int index) {
+    final _$actionInfo = _$_HomeControllerBaseActionController.startAction(
+        name: '_HomeControllerBase.switchMethodeForEdit');
+    try {
+      return super.switchMethodeForEdit(index);
+    } finally {
+      _$_HomeControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+isEditionAnotation: ${isEditionAnotation},
+items: ${items}
+    ''';
+  }
+}
