3aae16e086f9623fbcaa4ab6240ea086f456af21
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 14:36:53 -0300

    uso de changednotifier e adapterredirect
    - usado changednotifier para faciltar a injeção de dependencia
    - usado o adapterredirect para adaptar o redirect

--- prova_flutter/lib/src/repository/redirect/redirect.dart
+++ prova_flutter/lib/src/repository/redirect/redirect.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/widgets.dart';
 import 'package:prova_flutter/src/repository/redirect/adapterredirect.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class UrlLauncher implements AdapterRedirect  {
+class Redirect extends ChangeNotifier implements AdapterRedirect  {
   @override
-  Future<void> redirect(String url) async {
+  Future<void> redirectWEB(String url) async {
     if (!await launchUrl(Uri.parse(url))) {
       throw Exception('Could not launch $url');
     }
