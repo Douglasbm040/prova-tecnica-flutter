c1a5b8f2ed13a3c3c7c01033053b4df070e0b6da
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-18 14:45:42 -0300

    Update homepage.dart

--- prova_flutter/lib/src/views/home/page/homepage.dart
+++ prova_flutter/lib/src/views/home/page/homepage.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:prova_flutter/src/custom/customtheme.dart';
+import 'package:prova_flutter/src/repository/redirect/redirect.dart';
+import 'package:prova_flutter/src/views/home/components/listanotationcomponent.dart';
 import 'package:prova_flutter/src/views/home/controller/homecontroler.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:prova_flutter/src/views/shared/components/privacepolitictextcomponent.dart';
 
+import '../../../repository/persistence/persistence.dart';
+import '../../shared/controller/generalcontroller.dart';
+import '../components/textfieldcomponent.dart';
 import '../controller/anotationcontroler.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage(
+      {super.key,
+      required this.controllerGeneral,
+      required this.controllerPage});
+  final GeneralController controllerGeneral;
+  final HomeController controllerPage;
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final homeController = HomeController();
+  final homeController = HomeController(
+      textController: TextEditingController(),
+      sharedPreferences: SharedPreference());
+  final genralController = GeneralController(redirect: Redirect());
   final FocusNode _focusNode = FocusNode();
 
   @override
@@ -37,119 +48,14 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              SafeArea(
-                child: Card(
-                  child: SizedBox(
-                    width: width * .8,
-                    height: height * .55,
-                    child: Observer(builder: (context) {
-                      return homeController.items.isEmpty
-                          ? const Center(
-                              child: Text(
-                                  "Você ainda não adicionou anotações"),
-                            )
-                          : ListView.builder(
-                              itemCount: homeController.items.length,
-                              itemBuilder: (context, index) {
-                                final anotationController =
-                                    homeController.items[index];
-                                return Card(
-                                    elevation: 3,
-                                    child: Column(
-                                      children: [
-                                        SizedBox(
-                                          width: width * .8,
-                                          child: Stack(
-                                            children: [
-                                              Observer(builder: (context) {
-                                                return AnimatedContainer(
-                                                  curve: Curves.easeInOut,
-                                                  duration: const Duration(
-                                                      seconds: 1),
-                                                  width: 240,
-                                                  height: anotationController
-                                                          .anotation
-                                                          .isNotExpansed
-                                                      ? 80
-                                                      : 350,
-                                                  child: InkWell(
-                                                    onTap: anotationController
-                                                        .anotationChangeIsExpanded,
-                                                    child: Text(
-                                                      anotationController
-                                                          .anotation.text,
-                                                      maxLines:
-                                                          anotationController
-                                                                  .anotation
-                                                                  .isNotExpansed
-                                                              ? null
-                                                              : 3,
-                                                      overflow:
-                                                          TextOverflow.ellipsis,
-                                                    ),
-                                                  ),
-                                                );
-                                              }),
-                                              Positioned(
-                                                right: 10,
-                                                child: Row(
-                                                  children: [
-                                                    Padding(
-                                                      padding:
-                                                          const EdgeInsets.all(
-                                                              8.0),
-                                                      child: InkWell(
-                                                          onTap: () =>
-                                                              homeController
-                                                                  .switchMethodeForEdit(
-                                                                      index),
-                                                          child: const Icon(
-                                                              Icons.edit)),
-                                                    ),
-                                                    InkWell(
-                                                      onTap: () => homeController
-                                                          .showDeleteConfirmationDialog(
-                                                              context, index),
-                                                      child: const Icon(
-                                                        Icons.close_rounded,
-                                                        color: Colors.red,
-                                                      ),
-                                                    )
-                                                  ],
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ));
-                              });
-                    }),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 0,
-              ),
-              SizedBox(
-                width: width * 0.8,
-                child: TextField(
-                  controller: homeController.textController,
-                  focusNode: _focusNode,
-                  decoration: InputDecoration(
-                    hintText: "Digite seu nome",
-                    filled: true,
-                    fillColor: Theme.of(context).colorScheme.background,
-                    border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
-                  onSubmitted: homeController.editAnotationText,
-                ),
-              ),
+              ListAnotationComponent(
+                  width: width, height: height, homeController: homeController),
+              TextFieldComponent(
+                  width: width,
+                  homeController: homeController,
+                  focusNode: _focusNode),
               PrivacePoliticTextComponent(
-                redirect: () async {
-                  homeController.redirectWeb();
-                },
+                redirect: genralController.onTextPrivicyPolitic,
               ),
             ],
           ),
